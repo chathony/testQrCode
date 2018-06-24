24d52428525920af5ed85c998377d59b2c20d4a5
charles-anthony.pedoussaud@epitech.eu <charles-anthony.pedoussaud@epitech.eu>
2018-06-24 16:38:12 +0200

    xommit

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+  Alert,
+  Linking,
+  Dimensions,
+  LayoutAnimation,
+  Text,
+  View,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import { BarCodeScanner, Permissions } from 'expo';
+
+export default class App extends Component {
+  state = {
+    hasCameraPermission: null,
+    lastScannedUrl: null,
+  };
+
+  componentDidMount() {
+    this._requestCameraPermission();
+  }
+
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeRead = result => {
+    if (result.data !== this.state.lastScannedUrl) {
+      LayoutAnimation.spring();
+      var myStr = result.data;
+      myStr = myStr.substring(4, 17);
+      console.log('api/medoc/cip13/' + myStr);
+
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        {this.state.hasCameraPermission === null
+          ? <Text>Requesting for camera permission</Text>
+          : this.state.hasCameraPermission === false
+              ? <Text style={{ color: '#fff' }}>
+                  Camera permission is not granted
+                </Text>
+              : <BarCodeScanner
+                  onBarCodeRead={this._handleBarCodeRead}
+                  style={{
+                    height: Dimensions.get('window').height,
+                    width: Dimensions.get('window').width,
+                  }}
+                />}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
